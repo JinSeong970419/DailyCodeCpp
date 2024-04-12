2077f1345dd90fcc4c7a73af805933bfced40331
Jinseong Byeon <wlstjd2550@gmail.com>
2024-04-13 07:57:21 +0900

    Create Code1912.cpp
    백준 1912) 연속합
    - DP

---
+++ 그리디 라인스위핑 투포인터/Code1912.cpp
@@ -0,0 +1,17 @@
+﻿// 백준 1912 연속합
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int n, sum, a, ret = -1001;
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a);
+        sum += a;
+        ret = max(ret, sum);
+        if (sum < 0) sum = 0;
+    }
+    printf("%d\n", ret);
+    return 0;
+}
